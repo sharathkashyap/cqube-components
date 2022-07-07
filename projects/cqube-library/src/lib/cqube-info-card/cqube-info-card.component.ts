68b0cfdaf3f6cca52fc13024fcc280eac7b47e0f
Sharath Prasad <sharath@Sharaths-MacBook-Pro.local>
2022-07-07 16:52:19 +0530

    Issue #SB-0000:Cqube Placeholder creation

--- projects/cqube-library/src/lib/cqube-info-card/cqube-info-card.component.ts
+++ projects/cqube-library/src/lib/cqube-info-card/cqube-info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sb-cqube-info-card',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CqubeInfoCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() name: string = "";
+  @Input() value: string = "";
+  @Input() tooltip: string = "";
 
   ngOnInit(): void {
   }
